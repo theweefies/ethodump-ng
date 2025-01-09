b2862d5d8adf2b60117705778c8196e716acaecb
user name <user@laptop.com>
2025-01-09 20:52:47 +0000

    added fix for ssdp location lines that ommit host ip. Also added characterization of upnp service version to client services attribute if detected.

--- ssdp.py
+++ ssdp.py
@@ -6,9 +6,19 @@ from urllib.parse import urlparse
 from globals import Client, grab_resource, GENERIC_UPNP_UA
 from models import upnp_useragent_patterns
 
-NOTIFY = 'NOTIFY'
-SEARCH = 'M-SEARCH'
-UPNP_BASE_STRING = 'UPnP/1.0'
+NOTIFY               = 'NOTIFY'
+SEARCH               = 'M-SEARCH'
+UPNP_BASE_STRING     = 'UPnP/1.0'
+UPNP_1_1_BASE_STRING = 'UPnP/1.1'
+
+LOCATION_TAG         = 'LOCATION:'
+SERVER_TAG           = 'SERVER:'
+USER_AGENT_TAG       = 'USER-AGENT'
+
+DEVICE_TYPE_KEY      = "device_type"
+
+HTTP_NO_HOST         = 'http://:'
+HTTPS_NO_HOST        = 'https://:'
 
 @dataclass
 class SSDP:
@@ -24,9 +34,9 @@ def parse_user_agent(user_agent):
             # Extract data based on the named groups in the pattern
             for name, value in match.groupdict().items():
                 if value:
-                    return {name: value, "device_type": device_type}
+                    return {name: value, DEVICE_TYPE_KEY: device_type}
             # If no named groups or values, return the device type
-            return {"device_type": device_type}
+            return {DEVICE_TYPE_KEY: device_type}
     # Return None if no pattern matches
     return None
 
@@ -55,31 +65,37 @@ def parse_ssdp_packet(payload: bytes, cur_client: Client, grab_resources: bool=F
         location = ""
         server = ""
         for line in lines:
-            if 'LOCATION:' in line:
+            if LOCATION_TAG in line:
                 location = line.split(': ')[1].strip()
                 if location:
                     if grab_resources and location not in cur_client.resource_urls:
                             parsed_urn = urlparse(location)
                             if parsed_urn:
                                 grab_resource(location, GENERIC_UPNP_UA, parsed_urn)
+                    if HTTP_NO_HOST in location or HTTPS_NO_HOST in location:
+                        prefix, suffix = location.split('//')
+                        location = f"{prefix}//{cur_client.ip_address}{suffix}"
+                        print(location)
                     cur_client.resource_urls.add(location)
-            if 'SERVER:' in line:
+            if SERVER_TAG in line:
                 server = line.split(': ')[1].strip()
                 cur_client.user_agents.add(server)
                 ua_parse_result = parse_user_agent(server)
                 if ua_parse_result:
-                    device_type = ua_parse_result.get('device_type')
+                    device_type = ua_parse_result.get(DEVICE_TYPE_KEY)
                     if device_type:
                         cur_client.oses.add('dt: ' + device_type)
                 else:
                     os_regex = r"([A-Za-z]+\/[\d\.]+)"
                     matches = re.findall(os_regex, server)
                     match = matches[0] if matches else None
-                    if match and match != UPNP_BASE_STRING:
+                    if match and match not in [UPNP_BASE_STRING, UPNP_1_1_BASE_STRING]:
                         cur_client.oses.add('os: ' + match)
+                    elif match and match in [UPNP_BASE_STRING, UPNP_1_1_BASE_STRING]:
+                        cur_client.services.add(match)
                     else:
                         cur_client.oses.add('upnp: ' + server)
-            if 'USER-AGENT' in line:
+            if USER_AGENT_TAG in line:
                 user_agent = line.split(': ')[1].strip()
                 cur_client.user_agents.add(user_agent)
 
